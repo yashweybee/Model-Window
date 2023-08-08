943870eccef5ce4e7dc4d005cfe33d326aef7c2d
yashweybee <yashparmar@weybee.in>
2023-08-08 18:45:23 +0530

    project completed

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+const modal = document.querySelector(".modal")
+const overlay = document.querySelector(".overlay")
+const btnCLoseModel = document.querySelector(".close-modal")
+const btnOpenModel = document.querySelectorAll(".show-modal")
+
+const closeModal = () => {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+const openModal = ()=> {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+    btnOpenModel[i].addEventListener('click', openModal)
+}
+
+btnCLoseModel.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(e){
+    if(e.key === "Escape" && !modal.classList.contains('hidden')){
+            closeModal()
+    }
+})
